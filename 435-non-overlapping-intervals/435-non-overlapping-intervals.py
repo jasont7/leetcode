591b2c1251cbb502dfba708bd23e2d58929c9750
Jason Thomo <amjbvic@gmail.com>
2022-01-10 12:38:52 -0800

    Time: 2770 ms (6.80%), Space: 59.8 MB (57.19%) - LeetHub

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        intervals.sort(key=lambda i: i[1])
+        
+        A = []
+        for i in intervals:
+            if not len(A) or i[0] >= A[-1][1]:
+                A.append(i)
+        
+        return len(intervals) - len(A)
