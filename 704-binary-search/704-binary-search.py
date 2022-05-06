0b759c4c766f55690d4362c892666fef2e7d5f3f
Jason Thomo <amjbvic@gmail.com>
2022-05-05 17:26:09 -0700

    Time: 202 ms (81.79%), Space: 14.6 MB (55.17%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def search(self, nums, target):
         left, right = 0, len(nums)-1
-        while left <= right:
+        while left <= right: 
             pivot = left + (right-left)//2
             if nums[pivot] == target:
                 return pivot
