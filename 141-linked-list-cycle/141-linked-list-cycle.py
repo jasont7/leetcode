a7af8a4310cfacc3017666daec876fe275a8847d
Jason Thomo <amjbvic@gmail.com>
2022-05-07 17:41:36 -0700

    Time: 51 ms (68.00%), Space: 20.7 MB (25.24%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -3,14 +3,10 @@ class Solution(object):
         if not head: return False
         
         slow_ptr = head
-        fast_ptr = head.next
-        while fast_ptr:
-            if slow_ptr == fast_ptr:
-                return True
-            if not fast_ptr.next:
-                return False
-            
+        fast_ptr = head
+        while fast_ptr.next and fast_ptr.next.next:
             slow_ptr = slow_ptr.next
             fast_ptr = fast_ptr.next.next
+            if slow_ptr == fast_ptr: return True
         
         return False
