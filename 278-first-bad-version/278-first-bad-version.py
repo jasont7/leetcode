3da94829a4f78b1605bc6808a26388aab174402d
Jason Thomo <amjbvic@gmail.com>
2022-05-08 12:04:10 -0700

    Time: 20 ms (62.44%), Space: 13.5 MB (10.83%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -6,7 +6,7 @@
 class Solution(object):
     def firstBadVersion(self, n):
         left, right = 1, n
-        while left < right:
+        while left < right: 
             mid = left + (right-left) // 2
             if isBadVersion(mid):
                 right = mid
