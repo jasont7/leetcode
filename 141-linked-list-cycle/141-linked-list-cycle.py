4c3817fef1df4104aa4003924cdf0f35460be8dd
Jason Thomo <amjbvic@gmail.com>
2022-05-07 17:29:47 -0700

    Time: 80 ms (18.15%), Space: 21.6 MB (5.90%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -3,26 +3,15 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
 class Solution(object):
-    # O(1) space alg
     def hasCycle(self, head):
-        slow = fast = head
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-            if slow == fast: 
-                return True
-        return False
-    
-    
-    # # O(n) space alg
-    # def hasCycle(self, curr):
-    #     seen = {}
-    #     while curr and curr.next:
-    #         if curr.next.val in seen and seen[curr.next.val] == curr.next:
-    #             return True
-    #         seen[curr.val] = curr
-    #         curr = curr.next
-    #     return False
+        seen = {}
         
+        node = head
+        while node:
+            if node in seen:
+                return True
+            seen[node] = 1
+            node = node.next
+            
+        return False
