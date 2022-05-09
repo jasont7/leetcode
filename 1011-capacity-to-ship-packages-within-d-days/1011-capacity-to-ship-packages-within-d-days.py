0b0a27ccb1f65e5a83bf128e7903b80995874460
Jason Thomo <amjbvic@gmail.com>
2022-05-08 21:13:22 -0700

    Time: 552 ms (45.31%), Space: 15.1 MB (96.46%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def shipWithinDays(self, weights, D):
+        # checks if capacity can be shipped in D days
+        def feasible(capacity):
+            days = 1
+            total = 0
+            for weight in weights:
+                total += weight
+                if total > capacity:
+                    total = weight
+                    days += 1
+                    if days > D:
+                        return False
+            return True
+        
+        # binary search to find minimal feasible capacity
+        # max(weights) is the minimum possible capacity
+        # sum(weights) is the maximum possible capacity
+        left, right = max(weights), sum(weights)
+        while left < right:
+            mid = left + (right-left) // 2
+            if feasible(mid):
+                right = mid  # any capacity greater than mid is feasible
+            else:
+                left = mid + 1  # any capacity less than mid is not feasible
+        return left
