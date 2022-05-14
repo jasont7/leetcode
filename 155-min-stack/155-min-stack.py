a14fc4fedb15a6ec5778db96b09bf0bdf4756ac6
Jason Thomo <amjbvic@gmail.com>
2022-05-14 00:15:12 -0700

    Time: 63 ms (62.44%), Space: 19.4 MB (6.04%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -10,7 +10,7 @@ class MinStack(object):
 
     def push(self, val):
         if not self.head:
-            self.head = Node(val, val, None) 
+            self.head = Node(val, val, None)
         else:
             self.head = Node(val, min(val, self.head.min), self.head)
 
