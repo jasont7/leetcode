1d590bd33bc5fb0ae9e622d529f0f5069df0e7b3
Jason Thomo <amjbvic@gmail.com>
2022-05-14 00:43:19 -0700

    Time: 191 ms (40.35%), Space: 14.9 MB (31.07%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,12 +1,6 @@
 class Solution(object):
     def majorityElement(self, nums):
-        d = {}
-        for n in nums:
-            if n in d:
-                d[n] += 1
-            else:
-                d[n] = 1
-        
-        for n in d:
-            if d[n] > len(nums)//2: 
-                return n
+        counts = collections.Counter(nums)
+        for num in counts:
+            if counts[num] > len(nums)//2: 
+                return num
