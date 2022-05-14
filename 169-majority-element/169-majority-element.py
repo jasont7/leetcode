75b8ee6ea1e3d9dffa83f53d277c1d34100ea359
Jason Thomo <amjbvic@gmail.com>
2022-05-14 00:40:48 -0700

    Time: 196 ms (37.28%), Space: 14.9 MB (52.84%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        d = {}
+        for n in nums:
+            if n in d:
+                d[n] += 1
+            else:
+                d[n] = 1
+        
+        for n in d:
+            if d[n] > len(nums)//2:
+                return n
