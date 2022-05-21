0e3265eb430f81310ac1e9a316319b77778f7aa0
Jason Thomo <amjbvic@gmail.com>
2022-05-21 16:32:12 -0700

    Time: 24 ms (45.62%), Space: 13.4 MB (69.90%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def spiralOrder(self, A):
+        m, n = len(A), len(A[0])
+        
+        res = []
+        
+        top, left = 0, 0
+        bottom, right = len(A)-1, len(A[0])-1
+        while True:
+            for i in range(left, right+1):
+                res.append(A[top][i])
+            top += 1
+            if len(res) == n*m: break
+            
+            for i in range(top, bottom+1):
+                res.append(A[i][right])
+            right -= 1
+            if len(res) == n*m: break
+            
+            for i in range(right, left-1, -1):
+                res.append(A[bottom][i])
+            bottom -= 1
+            if len(res) == n*m: break
+            
+            for i in range(bottom, top-1, -1):
+                res.append(A[i][left])
+            left += 1
+            if len(res) == n*m: break
+            
+        return res
