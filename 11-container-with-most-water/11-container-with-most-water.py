04bdc511dcfedd937578ae697b85a4abe79324a3
Jason Thomo <amjbvic@gmail.com>
2022-05-21 12:28:50 -0700

    Time: 943 ms (18.34%), Space: 23.9 MB (53.90%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,12 +1,13 @@
 class Solution(object):
-    def maxArea(self, lines):
-        maxVol = 0
-        l, r = 0, len(lines)-1
+    def maxArea(self, height):
+        maxarea = 0
+        l, r = 0, len(height)-1
         while l < r:
-            curVol = min(lines[l], lines[r]) * (r-l)
-            maxVol = max(maxVol, curVol)
-            if lines[l] < lines[r]:
+            area = (r-l) * min(height[l], height[r])
+            maxarea = max(area, maxarea)
+            
+            if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-        return maxVol
+        return maxarea
