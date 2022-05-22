507348aabd9449df66b109e60907dae3253b4303
Jason Thomo <amjbvic@gmail.com>
2022-05-21 20:38:02 -0700

    Time: 18 ms (99.01%), Space: 13.8 MB (15.74%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -4,7 +4,7 @@ class Solution(object):
         left, right = 0, len(nums)-1
         while left < right:
             mid = (right + left)//2
-            if nums[mid] < nums[right]:
+            if nums[mid] < nums[right]: 
                 right = mid
             else:
                 left = mid + 1
