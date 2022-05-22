f6e85659372d2d321145aec1bdcbcb0b6a779309
Jason Thomo <amjbvic@gmail.com>
2022-05-22 00:47:26 -0700

    Time: 154 ms (21.24%), Space: 23.9 MB (96.28%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -1,63 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-class Solution(object):
-    
-    def reorderList(self, head):
-        """
-        :type head: ListNode
-        :rtype: None Do not return anything, modify head in-place instead.
-        """
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
         n = 0
-        p = head
-        while p.next:
-            n += 1
-            p = p.next
-        
-        t1, t2 = head, head.next
-        i = 0
-        while t2:
-            if i == n//2:
-                t1.next = None
-                break
-            t1, t2 = t2, t2.next
-            i += 1
-        
-        L1 = head
-        L2 = self.reverse(t2)
-        return self.merge(L1, L2)
-    
-    def reverse(self, head):
-        pre, cur = None, head
+        stack = []
+        cur = head
         while cur:
-            tmp = cur.next
-            cur.next = pre
-            pre, cur = cur, tmp
-        return pre
-    
-    def merge(self, a, b):
-        dummy = ListNode()
-        tail = dummy
-
-        while True:
-            if a is None:
-                tail.next = None
-                break
-            elif b is None:
-                tail.next = a
-                break
-            else:
-                tail.next = a
-                tail = a
-                a = a.next
-
-                tail.next = b
-                tail = b
-                b = b.next
-
-        a = dummy.next 
-        return a
+            stack.append(cur)
+            n += 1
+            cur = cur.next
         
+        cur = head
+        i = 0
+        while i < n//2:
+            new = stack.pop()
+            new.next = cur.next
+            cur.next = new
+            
+            i += 1
+            cur = cur.next.next
+        cur.next = None
