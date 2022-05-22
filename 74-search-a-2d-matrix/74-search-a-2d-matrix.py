5fd2898009146552bb088d350008b10ef34ddb36
Jason Thomo <amjbvic@gmail.com>
2022-05-21 20:18:18 -0700

    Time: 55 ms (14.71%), Space: 13.7 MB (75.45%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def searchMatrix(self, A, t):
+        def findRow():
+            left, right = 0, len(A)-1
+            while left <= right:
+                mid = (left + right) // 2
+                if A[mid][0] > t:
+                    right = mid-1
+                elif A[mid][-1] < t:
+                    left = mid+1
+                else:
+                    return A[mid]
+            return None
+        
+        def searchRow(row):
+            left, right = 0, len(row)-1
+            while left <= right:
+                mid = (left + right) // 2
+                if row[mid] > t:
+                    right = mid-1
+                elif row[mid] < t:
+                    left = mid+1
+                else:
+                    return True
+            return False
+        
+        row = findRow()
+        return searchRow(row) if row else False
