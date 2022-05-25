423986c9ba2e2a958a74fc2240d1f48cf21d7cbc
Jason Thomo <amjbvic@gmail.com>
2022-05-25 13:22:18 -0700

    Time: 19 ms (76.01%), Space: 13.2 MB (97.88%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -10,4 +10,4 @@ class Solution(object):
             return p.val == q.val and \
                 self.isSameTree(p.left, q.left) and \
                 self.isSameTree(p.right, q.right)
-        return p == q
+        return p == q 
