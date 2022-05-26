f282e91d58ccae222193df9b17b48c79e9f4797c
Jason Thomo <amjbvic@gmail.com>
2022-05-26 12:43:38 -0700

    Time: 633 ms (73.37%), Space: 16.8 MB (63.55%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,29 +1,26 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def threeSum(self, nums):
         nums.sort()
         
-        triplets = []
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i-1]:  # skip repeat targets
+        res = []
+        for i in range(0, len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-
-            # we want to keep these two pointers ahead of the target
-            # so as not to have a repeating sequence of pointers.
-            l, r = i+1, len(nums)-1
-            
-            while l < r:
-                s = nums[i] + nums[l] + nums[r]  # nums[i] is the target
                 
-                if   s < 0: l += 1
-                elif s > 0: r -= 1
-                elif s == 0:
-                    triplets.append([nums[i], nums[l], nums[r]])
-                    
-                    # skip repeat numbers for the given target
-                    while l < r and nums[l] == nums[l+1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r-1]:
-                        r -= 1
-                    l += 1; r -= 1
+            target = -nums[i]
+            lo, hi = i+1, len(nums)-1
+            while lo < hi:
+                if nums[lo] + nums[hi] == target:
+                    res.append([nums[i], nums[lo], nums[hi]])
+                    while lo < hi and nums[lo] == nums[lo+1]:
+                        lo += 1
+                    while lo < hi and nums[hi] == nums[hi-1]:
+                        hi -= 1
+                    lo += 1
+                    hi -= 1
+                elif nums[lo] + nums[hi] < target:
+                    lo += 1
+                else:
+                    hi -= 1
         
-        return triplets
+        return res
